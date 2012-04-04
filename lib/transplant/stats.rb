d636ce4463dabb268c157a7694e58cf7716bbea0
Matt Bridges <mbridges.91@gmail.com>
2012-04-04 11:36:14 -0500

    Updating references to method name changes and a few other refactors

--- lib/transplant/stats.rb
+++ lib/transplant/stats.rb
@@ -2,13 +2,14 @@ module Transplant
   class Stats
 
     def initialize(transplanter)
-      @transplanter = transplanter
+      @planter = transplanter
     end
 
     def output_all_info(opts = {})
       total_import_time(opts[:measurement]) if opts[:measurement].present?
       total_import_records
-      @transplanter.failures
+      successes
+      failures
     end
 
     class << self
@@ -27,7 +28,7 @@ module Transplant
           if value.is_a?(Hash) || value.is_a?(Array)
             output(key, value, depth + 1, sub_hash = true)
           else
-            puts tabs(depth + 1) + "#{key}: #{value}"
+            puts tabs(depth + 1) + "* #{key}: #{value}"
           end
         end
       end
@@ -51,19 +52,28 @@ module Transplant
     end
 
     def total_import_records
-      puts "Estimated number of records imported into #{@transplanter.app_name}: #{@transplanter.total_records}"
+      puts "Estimated number of records imported into #{@planter.app_name}: #{@planter.total_successes}"
     end
 
-    def total_import_time(measurement)
-      puts "Total time taken to import everything into #{@transplanter.app_name}: #{(measurement.real/60).round(2)} minutes"
+    def total_import_time(m)
+      puts "Total time taken to import everything into #{@planter.app_name}: #{(m.real/60).round(2)} minutes"
+    end
+
+    def successes
+      if @planter.successes.count <= 0
+        puts "\nNo records were imported! Boo!!!!\n"
+      else
+        count = Hash[@planter.successes.map { |key, value| [key.tableize.humanize, value] }]
+        self.class.output("\nEstimated number of successful imports to #{@planter.app_name}", count)
+      end
     end
 
     def failures
-      if @transplanter.failures.count <= 0
+      if @planter.failures.count <= 0
         puts "\nNo failed record imports!!!! Time to par-tay!!!!\n"
       else
-        failures = Hash[@transplanter.failures.map { |key, value| [key.tableize.humanize, value] }]
-        self.class.output("\nTotal number failed imports to #{@transplanter.app_name}:", failures)
+        count = Hash[@planter.failures.map { |key, value| [key.tableize.humanize, value] }]
+        self.class.output("\nEstimated number failed imports to #{@planter.app_name}", count)
       end
     end
 
