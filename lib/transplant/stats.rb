4ab46b43d5adbeb58b5924ae07bdbc013c48862a
Matt Bridges <mbridges.91@gmail.com>
2012-04-04 11:00:25 -0500

    Refactoring the Stats class to be DRYer

--- lib/transplant/stats.rb
+++ lib/transplant/stats.rb
@@ -11,38 +11,43 @@ module Transplant
       @transplanter.failures
     end
 
-    def self.output(header, input, depth = 0, sub_output = false)
-      if input.is_a? Hash
-        hash_output(header, input, depth, sub_output)
-      elsif input.is_a? Array
-        array_output(header, input, depth, sub_output)
-      end
-    end
+    class << self
 
-    def self.hash_output(header, hash, depth = 0, sub_hash = false)
-      puts "#{"\t"*depth}#{header}:" if hash.any?
-      hash.each_pair do |key, value|
-        if value.is_a? Hash
-          hash_output(key, value, depth + 1, sub_hash = true)
-        elsif value.is_a? Array
-          array_output("", value, depth + 1, sub_array = true)
-        else
-          puts "#{"\t"*(depth + 1)}#{key}: #{value}"
+      def output(header, input, depth = 0, sub_output = false)
+        if input.is_a? Hash
+          hash_output(header, input, depth, sub_output)
+        elsif input.is_a? Array
+          array_output(header, input, depth, sub_output)
         end
       end
-    end
 
-    def self.array_output(header, array, depth = 0, sub_array = false)
-      puts "#{"\t"*depth}#{header}:" if array.any?
-      array.each do |item|
-        if item.is_a? Array
-          array_output("", item, depth + 1, sub_array = true)
-        elsif item.is_a? Hash
-          hash_output("", item, depth + 1, sub_hash = true)
-        else
-          puts "#{"\t"*(depth + 1)}* #{item}"
+      def hash_output(header, hash, depth = 0, sub_hash = false)
+        puts tabs(depth) + "#{header}:" if hash.any? && header.present?
+        hash.each_pair do |key, value|
+          if value.is_a?(Hash) || value.is_a?(Array)
+            output(key, value, depth + 1, sub_hash = true)
+          else
+            puts tabs(depth + 1) + "#{key}: #{value}"
+          end
         end
       end
+
+      def array_output(header, array, depth = 0, sub_array = false)
+        puts tabs(depth) + "#{header}:" if array.any? && header.present?
+        array.each do |item|
+          if item.is_a?(Hash) || item.is_a?(Array)
+            output("", item, depth + 1, sub_hash = true)
+          else
+            puts tabs(depth + 1) + "* #{item}"
+          end
+        end
+      end
+
+      def tabs(count)
+        "\t"*count
+      end
+      private :tabs
+
     end
 
     def total_import_records
