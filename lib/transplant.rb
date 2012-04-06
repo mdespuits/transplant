e6a4bcc3d4fdb7e43178d548c05e15d6f5713348
Matt Bridges <mbridges.91@gmail.com>
2012-04-06 18:48:55 -0500

    load files more concisely I would use autoload but for its thread-unsafeness

--- lib/transplant.rb
+++ lib/transplant.rb
@@ -1,9 +1,5 @@
 require 'active_support/all'
 
-require "transplant/version"
-require "transplant/configuration"
-require "transplant/stats"
-require "transplant/planter"
-
-module Transplant
+%w[version configuration stats planter].each do |klass|
+  require "transplant/#{klass}"
 end
