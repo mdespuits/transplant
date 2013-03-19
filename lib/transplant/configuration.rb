24248b60921fcdac60878395b7d8db1c6eb83b17
Matt Bridges <mbridges.91@gmail.com>
2013-03-19 18:17:00 -0400

    do not need active_support class core ext

--- lib/transplant/configuration.rb
+++ lib/transplant/configuration.rb
@@ -1,19 +1,11 @@
-require 'active_support/core_ext/class'
-
 module Transplant
   class Configuration
-
-    cattr_accessor :root_path
-    cattr_accessor :output_style
-    cattr_accessor :file_output_format
-
     class << self
+      attr_accessor :root_path, :output_style, :file_output_format
 
       def setup
         yield self
       end
-
     end
-
   end
 end
